226b6f2b1d0497d5f45d4da8e8be370aad61a04c
ddfd@sdsd.sd <ddfd@sdsd.sd>
2019-01-09 14:33:42 +0700

    up

---
+++ danhsachKH.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script></head>
+<body>
+<div id="wrapper">
+    <div class="row">
+        <div class="col-md-12 border" id="header">
+            <h1>This is header</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 border">
+            <ul class="nav">
+
+                <li class="nav-item"><a href="#" class="nav-link">home</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">danhsach</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">them</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-3 border">
+            <ul class="nav flex-column">
+                <li class="nav-item"><a href="#" class="nav-link">danh sach</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">sua</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">them moi</a></li>
+            </ul>
+        </div>
+        <div class="col-md-9 border">
+            <h3>Danh Sach Khach Hang</h3>
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Ten</th>
+                    <th>SDT</th>
+                    <th>email</th>
+                    <th>diachi</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Hoa</td>
+                    <td>0928282</td>
+                    <td>sd@sds.com</td>
+                    <td>THai Nguyen</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Huong</td>
+                    <td>0928282</td>
+                    <td>sd@sds.com</td>
+                    <td>THai Nguyen</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 border">@2018 copyright mysite</div>
+    </div>
+</div>
+</body>
+</html>
---
+++ layout.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div id="wrapper">
+    <div class="row">
+        <div class="col-md-12 border" id="header">
+            <h1>This is header</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 border">
+            <ul class="nav">
+
+                <li class="nav-item"><a href="#" class="nav-link">home</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">danhsach</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">them</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-3 border">
+            <ul class="nav flex-column">
+                <li class="nav-item"><a href="#" class="nav-link">danh sach</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">sua</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">them moi</a></li>
+            </ul>
+        </div>
+        <div class="col-md-6 border">body</div>
+        <div class="col-md-3 border">
+            block phai
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 border">@2018 copyright mysite</div>
+    </div>
+</div>
+</body>
+</html>
